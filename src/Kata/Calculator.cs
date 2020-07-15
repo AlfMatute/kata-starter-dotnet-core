6e431350e9a8ab2cdbf4463c9041abb6abf06e06
AlfMatute <alf.matute@gmail.com>
2020-07-15 13:02:19 -0600

    Unit test fail 2

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -6,10 +6,6 @@ namespace Kata
     {
         public int Add(string number = "")
         {
-            if (!String.IsNullOrEmpty((number)))
-            {
-                return Int32.Parse(number);   
-            }
             return 0;
         }
     }
